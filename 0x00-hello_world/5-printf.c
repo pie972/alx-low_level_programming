f8ff69f5fc5eb8bd627cbe9cc61a73c01a4ab5f9
Pie Pie <i.jalal@aui.ma>
2023-03-16 15:03:02 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-:%s/\s\+$//g
 #include <stdio.h>
 
 /**
@@ -12,17 +10,3 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-=======
-#include <stdio.h>
-
-/**
-*main - Entry point
-*Return: Always 0 (Success)
-*/
-
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}
->>>>>>> 08a2ab89de2a279f3718aedf2faa8f53a54f2c54
