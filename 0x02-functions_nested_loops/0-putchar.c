8427672a2e1233dc6285062261ce5b2fe483d941
Pie Pie <i.jalal@aui.ma>
2023-03-21 17:50:59 +0000

    Update 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "_putchar.h"
+/**
+* main - prints _putchar
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i = 0;
+char s[] = "_putchar\n";
+while (s[i])
+{
+_putchar (s[i]);
+i++;
+}
+return (0);
+}
