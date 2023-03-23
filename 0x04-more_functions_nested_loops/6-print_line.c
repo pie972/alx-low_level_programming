987a1ae681359e442cb07b17912847bbd389e901
Pie Pie <i.jalal@aui.ma>
2023-03-23 11:59:29 +0000

    Update 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line using underscores
+ * @n: the number of underscores to print
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+		_putchar('_');
+
+	_putchar('\n');
+}
