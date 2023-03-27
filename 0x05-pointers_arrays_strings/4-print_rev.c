8de840bf48242d3a80b84e36be0afa9eb9cb31d7
Pie Pie <i.jalal@aui.ma>
2023-03-27 04:39:34 +0000

    Update 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: the string to print
+ */
+void print_rev(char *s)
+{
+	int length;
+
+for (length = 0; *s != '\0'; s++)
+length++;
+
+s--;
+length--;
+
+for (; length >= 0; length--, s--)
+	_putchar(*s);
+
+	_putchar('\n');
+}
