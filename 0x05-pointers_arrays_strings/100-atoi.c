413cb20907386015ae6059cde90e1609601012eb
Pie Pie <i.jalal@aui.ma>
2023-03-27 05:05:46 +0000

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,50 +2,50 @@
 #include <limits.h>
 
 /**
- * _atoi - convert a string to an integer
- * @s: the string to convert
+ * _atoi - Convert a string to an integer
+ * @s: Pointer to the string to convert
  *
- * Return: result of conversion
+ * Return: The integer that was converted
  */
 int _atoi(char *s)
 {
-	unsigned int n = 0;
-	int sign = 1;
-	int flag = 0;
+	int i, start, negative;
+	unsigned int number;
 
-	while (*s)
+	start = -1;
+	negative = 0;
+	number = 0;
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*s >= '0' && *s <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
+			start = i;
+
+		if (s[i] == '-' && start == -1)
 		{
-			flag = 1;
-			if (sign > 0)
-			{
-				if (INT_MAX / 10 < (int) n)
-					return (INT_MAX);
-				n *= 10;
-				if (INT_MAX - (*s - '0') < (int) n)
-					return (INT_MAX);
-			}
+			if (negative)
+				negative = 0;
 			else
-			{
-				if (INT_MIN / 10 < (int) -n)
-					return (INT_MIN);
-				n *= 10;
-				if (INT_MIN + (*s - '0') > (int) -n)
-					return (INT_MIN);
-			}
-			n += (*s - '0');
+				negative = 1;
 		}
-		else if (flag)
-		{
-			break;
-		}
-		else if (*s == '-')
-		{
-			sign *= -1;
-		}
-		++s;
 	}
 
-	return (n * sign);
+	if (start != -1)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			if (s[i] >= '0' && s[i] <= '9')
+				number = number * 10 + s[i] - '0';
+			else if (s[i - 1] >= '0' && s[i - 1] <= '9')
+				break;
+		}
+	}
+
+	if ((number >= (unsigned int) INT_MAX && negative == 0))
+		return (INT_MAX);
+	else if ((number > (unsigned int) INT_MAX && negative))
+		return (INT_MIN);
+	else if (negative)
+		return ((int) -number);
+	else
+		return ((int) number);
 }
