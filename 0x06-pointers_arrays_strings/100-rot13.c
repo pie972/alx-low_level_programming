ffb6a516275b7f27f71975b3fe9519c2abe0ecdb
Pie Pie <i.jalal@aui.ma>
2023-03-29 12:58:50 +0000

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,35 +1,37 @@
 #include "main.h"
 
 /**
- * rot13 - rotate 13 characters
+ * rot13 - Encode a string using rot13
+ * @s: The string to encode
  *
- * @a: value from main
- *
- * Return: value of a
+ * Return: The pointer to the string
  */
-
-char *rot13(char *a)
+char *rot13(char *s)
 {
+	int i, j;
+	char normal[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+			 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+			 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+			 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+			 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-	int outer;
-	int inner;
-	char rotation[55] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	char alphabet[55] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rotate[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+			 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+			 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't',
+			 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e',
+			 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 
-	for (outer = 0; a[outer] != '\0'; outer++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-
-		for (inner = 0; alphabet[inner] != '\0'; inner++)
+		for (j = 0; j < 52; j++)
 		{
-			if (a[outer] == alphabet[inner])
+			if (s[i] == normal[j])
 			{
-				a[outer] = rotation[inner];
-				break;
+				s[i] = rotate[j];
+				j = 52;
 			}
 		}
-
 	}
 
-	return (a);
-
+	return (s);
 }
