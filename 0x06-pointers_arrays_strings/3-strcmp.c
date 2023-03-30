51995f1b9afb88d18eeba23221429dec138a2761
Pie Pie <i.jalal@aui.ma>
2023-03-30 02:43:51 +0200

    tired

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,19 @@
  *
  * Return: value of difference
  */
-int _strcmp(char *s1, char *s2) {
-    while (*s1 != '\0' && *s2 != '\0') {
-        if (*s1 != *s2) {
-            return (*s1 < *s2) ? -1 : 1;
-        }
-        s1++;
-        s2++;
-    }
-    return (*s1 == '\0' && *s2 == '\0') ? 0 : (*s1 < *s2) ? -1 : 1;
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+	return (*s1 - *s2);
+	}
 }
