8c62b96223b42a28a830a862f1eefe3a0f63117f
Pie Pie <i.jalal@aui.ma>
2023-04-03 16:59:11 +0000

    Update 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+/**
+ * set_string - set value of a pointer to char
+ *
+ * @s: value from main
+ * @to: value from main
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
