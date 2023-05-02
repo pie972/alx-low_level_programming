aa85b6a164f1d30db27439fcafa215c28f8fc056
Pie Pie <i.jalal@aui.ma>
2023-05-02 22:47:56 +0100

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * reverse_listint - prints a listint_t linked list.
+ * print_listint_safe - prints a listint_t linked list.
  * @head: pointer to the list.
  * Return: number of nodes in the list.
  **/
